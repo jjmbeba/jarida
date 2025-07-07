c94665567ac9aebe7d9dd3a7ce8946f15c022254
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-07-07 15:45:06 +0300

    format index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
 // src/routes/index.tsx
-import { Button } from '@/components/ui/button';
 import { createFileRoute } from '@tanstack/react-router';
+
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/')({
   component: Home,
 });
 
 function Home() {
-  return <div className="font-bold text-3xl text-blue-500 underline">
-    <Button>
-      Hello
-    </Button>
-  </div>;
+  return (
+    <div className="font-bold text-3xl text-blue-500 underline">
+      <Button>Hello</Button>
+    </div>
+  );
 }
