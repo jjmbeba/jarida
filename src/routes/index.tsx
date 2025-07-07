4173a72c64cb2f2b5c086964697c3abdea128592
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-07-07 03:06:19 +0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+// src/routes/index.tsx
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+})
+
+function Home() {
+  return (
+    <div>
+      Home page
+    </div>
+  )
+}
