bba16c7a567171452dbe216311a302a48f406713
Johnson Mbeba <jjmbeba@gmail.com>
2025-07-07 22:38:51 +0300

    set hosting to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart()],
+  plugins: [tsConfigPaths(), tanstackStart({ target: 'vercel' })],
 });
