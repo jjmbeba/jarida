b1dc11c7d255f5ed4f67ab522a583a09e9056152
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-07-08 14:28:53 +0300

    edit vs code settings

--- src/routes/_protected/protected.tsx
+++ src/routes/_protected/protected.tsx
@@ -1,28 +1,30 @@
-import { convexQuery } from '@convex-dev/react-query'
-import { useQuery } from '@tanstack/react-query'
-import { createFileRoute } from '@tanstack/react-router'
-import { api } from 'convex/_generated/api'
+import { convexQuery } from '@convex-dev/react-query';
+import { useQuery } from '@tanstack/react-query';
+import { createFileRoute } from '@tanstack/react-router';
+import { api } from 'convex/_generated/api';
 
 export const Route = createFileRoute('/_protected/protected')({
   component: RouteComponent,
   loader: async ({ context }) => {
     await context.queryClient.ensureQueryData(
       convexQuery(api.tags.listTags, {})
-    )
-  }
-})
+    );
+  },
+});
 
 function RouteComponent() {
-  const { data: tags, isLoading } = useQuery(convexQuery(api.tags.listTags, {}));
+  const { data: tags, isLoading } = useQuery(
+    convexQuery(api.tags.listTags, {})
+  );
 
-  return <div>
-    <h1>Protected Route</h1>
-    {isLoading ? (
-      <div>Loading...</div>
-    ) : (
-      tags?.map((tag) => (
-        <div key={tag._id}>{tag.name}</div>
-      ))
-    )}
-  </div>
+  return (
+    <div>
+      <h1>Protected Route</h1>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        tags?.map((tag) => <div key={tag._id}>{tag.name}</div>)
+      )}
+    </div>
+  );
 }
